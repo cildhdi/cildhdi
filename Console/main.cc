bf00270fb9a37e95c94afe577a5a4a2e97e7cb9d
cildhdi <1961464399@qq.com>
2018-11-13 22:30:52 +0800

    ..

---
+++ Console/main.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <sstream>
+#include <map>
+#include <cstring>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <functional>
+#include <iterator>
+#include <sstream>
+#include <numeric>
+#include <gtest/gtest.h>
+
+using namespace testing;
+
+TEST(Cl, Void)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
---
+++ Cs/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Cs
+{
+    using Ionic.Zip;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Reflection;
+
+    namespace Wox.Plugin.PluginBackup
+    {
+        public static class GlobalState
+        {
+            private static string _plugin_name = "Wox.Plugin.PluginBackup";
+            private static string _plugin_path;
+            private static string _wox_path;
+            private static string _desktop_path;
+            private static string _backup_file_name = "Wox.Plugins.zip";
+
+            public static void Init()
+            {
+                UriBuilder uri = new UriBuilder(Assembly.GetExecutingAssembly().CodeBase);
+                _plugin_path = Path.GetDirectoryName(Uri.UnescapeDataString(uri.Path));
+                _wox_path = _plugin_path.Remove(_plugin_path.LastIndexOf(Path.DirectorySeparatorChar));
+                _wox_path = _wox_path.Remove(_wox_path.LastIndexOf(Path.DirectorySeparatorChar));
+
+                _desktop_path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+            }
+            public static string PluginName() { return _plugin_name; }
+
+            public static string ThisPluginDir() { return _plugin_path; }
+
+            public static string WoxDir() { return _wox_path; }
+
+            public static string WoxPluginsDir() { return _wox_path + Path.DirectorySeparatorChar + "Plugins"; }
+
+            public static string WoxSettingsDir() { return _wox_path + Path.DirectorySeparatorChar + "Settings"; }
+
+            public static string DesktopDir() { return _desktop_path; }
+
+            public static string BackupFileName() { return _backup_file_name; }
+        }
+
+        public class Main
+        {
+            public void Init()
+            {
+                GlobalState.Init();
+            }
+
+            public void Backup(string path)
+            {
+                path = path.Length == 0 ? GlobalState.DesktopDir() + Path.DirectorySeparatorChar + GlobalState.BackupFileName() : path;
+                if (File.Exists(path))
+                {
+                    try
+                    {
+                        File.Delete(path);
+                    }
+                    catch (System.IO.IOException e)
+                    {
+                        return;
+                    }
+                }
+                ZipFile zip = new ZipFile();
+                AddDirToZip(zip, GlobalState.WoxPluginsDir());
+                AddDirToZip(zip, GlobalState.WoxSettingsDir());
+                zip.Save(path);
+            }
+
+            private void AddDirToZip(ZipFile zip, string dir)
+            {
+                foreach (string file in Directory.GetFiles(dir))
+                    zip.AddFile(file, dir.Replace(GlobalState.WoxDir(), ""));
+                foreach (string sub in Directory.GetDirectories(dir))
+                    AddDirToZip(zip, sub);
+            }
+
+            public void Restore(string path)
+            {
+                path = path.Length == 0 ? GlobalState.DesktopDir() + Path.DirectorySeparatorChar + GlobalState.BackupFileName() : path;
+                ZipFile zip = ZipFile.Read(path);
+                List<string> existed_plugins = new List<string>();
+                foreach (string pd in Directory.GetDirectories(GlobalState.WoxPluginsDir()))
+                {
+                    if (pd.Contains("-"))
+                        existed_plugins.Add(pd.Remove(pd.IndexOf('-')).Replace(GlobalState.WoxPluginsDir() + Path.DirectorySeparatorChar, ""));
+                    else
+                        existed_plugins.Add(pd.Replace(GlobalState.WoxPluginsDir() + Path.DirectorySeparatorChar, ""));
+                }
+                foreach (ZipEntry entry in zip)
+                {
+                    string filename = entry.FileName.Remove(entry.FileName.IndexOf('-')).Remove(0, entry.FileName.IndexOf('/') + 1);
+                    if (!existed_plugins.Contains(filename))
+                        
+                        entry.Extract(GlobalState.WoxDir());
+                }
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+            }
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Wox.Plugin.PluginBackup.Main main = new Wox.Plugin.PluginBackup.Main();
+            main.Init();
+            //main.Backup("");
+            main.Restore("");
+            Console.ReadKey();
+        }
+    }
+}
