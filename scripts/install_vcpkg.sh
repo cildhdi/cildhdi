95a873a3bd4c74d36dfaa498cf1315fbd3e4ff1a
cildhdi <1961464399@qq.com>
2018-11-21 18:48:02 +0800

    use https

--- scripts/install_vcpkg.sh
+++ scripts/install_vcpkg.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-git clone git@github.com:Microsoft/vcpkg.git
+git clone https://github.com/Microsoft/vcpkg.git
 ./vcpkg/bootstrap-vcpkg.sh
 
 # install snowhouse and igloo by vcpkg
