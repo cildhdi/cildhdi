59b93260c12b7c646d6c574ba93175e9629b1f12
cildhdi <1961464399@qq.com>
2018-11-21 20:22:41 +0800

    rename

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,7 +9,7 @@ echo "\nString_unittest begin"
 ./build/String/String_unittest
 
 echo "\nga-de-ry-po-lu-ki-cypher-vol-2 begin"
-./build/ga-de-ry-po-lu-ki-cypher-vol-2/main
+./build/ga-de-ry-po-lu-ki-cypher-vol-2/ga-de-ry-po-lu-ki-cypher-vol-2
 
 echo "\nrainfall begin"
-./build/rainfall/main
+./build/rainfall/rainfall
