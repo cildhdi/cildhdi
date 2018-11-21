2d5086e0b0c9325bafcb44c2c55c839e64639508
cildhdi <1961464399@qq.com>
2018-11-21 19:44:16 +0800

    .

--- scripts/install_igloo.sh
+++ scripts/install_igloo.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 git clone https://github.com/joakimkarlsson/igloo.git
-
+git clone https://github.com/banditcpp/snowhouse.git "igloo/igloo/external/snowhouse"
 # install snowhouse and igloo
+
 sudo cp -a igloo/igloo /usr/include/
