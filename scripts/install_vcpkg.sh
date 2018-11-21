18c95e6cfd0044c356f2b5e81c2b3fa197a6752f
cildhdi <1961464399@qq.com>
2018-11-21 19:08:33 +0800

    .

--- scripts/install_vcpkg.sh
+++ scripts/install_vcpkg.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
 
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
+sudo apt-get update -y
+sudo apt-get install g++-7 -y
+
 git clone https://github.com/Microsoft/vcpkg.git
 ./vcpkg/bootstrap-vcpkg.sh
 
