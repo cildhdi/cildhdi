dcf9e6354b9e59efe4a9a8965097846fb58e0275
cildhdi <1961464399@qq.com>
2018-11-23 14:07:55 +0800

    change path

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -9,10 +9,10 @@ echo "\nString_unittest begin"
 ./build/String/String_unittest
 
 echo "\nga-de-ry-po-lu-ki-cypher-vol-2 begin"
-./build/ga-de-ry-po-lu-ki-cypher-vol-2/ga-de-ry-po-lu-ki-cypher-vol-2
+./build/codewars/ga-de-ry-po-lu-ki-cypher-vol-2/ga-de-ry-po-lu-ki-cypher-vol-2
 
 echo "\nrainfall begin"
-./build/rainfall/rainfall
+./build/codewars/rainfall/rainfall
 
 echo "\nstring-pyramid begin"
-./build/string-pyramid/string-pyramid
+./build/codewars/string-pyramid/string-pyramid
